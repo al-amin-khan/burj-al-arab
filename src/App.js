b81e2b0b50242b934dc7b7ca17d2c7f7eacb4dfc
al-amin-khan <alaminkhan2010@gmail.com>
2020-09-24 19:50:38 +0600

    firebaase google sign api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { useState, createContext } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,8 +10,16 @@ import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
 
+export const UserContext = createContext();
+
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p>Name: {loggedInUser.name}</p>
+      <p>Email: {loggedInUser.email}</p>
+
       <Router>
           <Header/>
           <Switch>
@@ -29,6 +37,8 @@ function App() {
             </Route>
           </Switch>
       </Router>
+    </UserContext.Provider>
+
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,49 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config"
+import {UserContext} from '../../App'
 
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  if(firebase.apps.length === 0){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  const handleGoogleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      // ...
+
+      const {displayName, email} = result.user;
+      const signedInUser = {name: displayName, email};
+      setLoggedInUser(signedInUser);
+
+      console.log(signedInUser);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+
+  }
+  return (<div>
+    <h1>This is Login</h1>
+    <button onClick={handleGoogleSignIn}>Google Sign in</button>
+  </div>);
 };
 
 export default Login;
